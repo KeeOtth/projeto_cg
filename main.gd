a8f64e632b7506da6b6f1587cd1c972f93597c56
Davi Romão <davi.romao0210@gmail.com>
2025-05-12 23:43:08 -0300

    okokok

--- main.gd
+++ main.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
 
-
+var players = []
+var current_index = 0
 
 func _on_blue_team_gol_body_entered(body: Node3D) -> void:
 	print("Red fez Gol")
@@ -11,3 +12,25 @@ func _on_blue_team_gol_body_entered(body: Node3D) -> void:
 func _on_red_team_gol_body_entered(body: Node3D) -> void:
 	print("Blue fez Gol")
 	pass # Replace with function body.
+
+
+func _ready():
+	# Encontra todos os jogadores (assumindo grupo "Players")
+	players = get_tree().get_nodes_in_group("Players")
+	if players.size() > 0:
+		players[0].is_active = true
+
+func _process(delta):
+	if Input.is_action_just_pressed("switch_player"):
+		# Desativa o atual
+		players[current_index].is_active = false
+
+		if players[current_index].held_ball:
+			players[current_index].held_ball.freeze = false
+			players[current_index].held_ball = null
+		
+		# Avança para o próximo
+		current_index = (current_index + 1) % players.size()
+
+		# Ativa o novo
+		players[current_index].is_active = true
--- player.gd
+++ player.gd
@@ -4,11 +4,18 @@ extends CharacterBody3D
 @export var speed = 8
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+@export var is_active = false
 
 var target_velocity = Vector3.ZERO
-var held_ball: RigidBody3D = null
+@export var held_ball: RigidBody3D = null
 
 func _physics_process(delta):
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	$Pivot/DirectionalIndicator.visible = is_active
+	if not is_active:
+		return
+	
 	var direction = Vector3.ZERO
 
 	if Input.is_action_pressed("move_back"):
@@ -29,10 +36,6 @@ func _physics_process(delta):
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-
-	# Vertical Velocity
-	if not is_on_floor():
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
 	for i in get_slide_collision_count():
 			var collision = get_slide_collision(i)
@@ -60,7 +63,6 @@ func _physics_process(delta):
 					var to_ball = (ball.global_transform.origin - player_pos)
 					to_ball.y = 0
 					var distance = to_ball.length()
-					print(distance)
 					var direction_to_ball = to_ball.normalized()
 					var dot = player_dir.dot(direction_to_ball)
 					
@@ -71,7 +73,7 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("ui_accept") and held_ball:
 		held_ball.freeze = false
-		var hold_pos = global_transform.origin + $Pivot.transform.basis.z * -1.6 + Vector3(0, 0.4, 0)
+		var hold_pos = global_transform.origin + $Pivot.transform.basis.z * -1.6 + Vector3(0, 0.37, 0)
 		held_ball.global_transform.origin = hold_pos
 		var impulse_direction = -$Pivot.basis.z.normalized()
 		var impulse_strength = 20.0
@@ -79,7 +81,7 @@ func _physics_process(delta):
 		held_ball = null
 
 	if held_ball:
-		var hold_pos = global_transform.origin + $Pivot.transform.basis.z * -1.2 + Vector3(0, 0.4, 0)
+		var hold_pos = global_transform.origin + $Pivot.transform.basis.z * -1.2 + Vector3(0, 0.37, 0)
 		held_ball.global_transform.origin = hold_pos
 
 	# Moving the Character
